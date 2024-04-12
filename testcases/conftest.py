1066b1df35760c44a78a246499aa951672682472
eitdj <163746639+eitdj@users.noreply.github.com>
2024-04-12 09:33:27 +0530

    Set up CI with Azure Pipelines
    [skip ci]

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -6,14 +6,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome",
+    )
+
+
 @pytest.fixture(autouse=True)
 def launch(request):
-    serv = Service("C:\\Users\\EIT\\Pictures\\j\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
-    driver = webdriver.Chrome(service=serv)
-    driver.delete_all_cookies()
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
     driver.get("https://naveenautomationlabs.com/opencart/")
-
-    driver.maximize_window()
+    driver.delete_all_cookies()
     request.cls.driver = driver
     yield
     driver.quit()
